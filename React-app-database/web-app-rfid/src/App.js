d05e65e7bb66485ec8bc6d0a0913196ac0d4fecd
gregvovtchenko <vovchenko2000@gmail.com>
2023-11-06 13:00:45 -0500

    upload backend and frontend

---
+++ React-app-database/web-app-rfid/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NFCManager from './components/NFCManager';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NFC Manager</h1>
+      </header>
+      <main>
+        <NFCManager />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-app-database/web-app-rfid/src/setupProxy.js
@@ -0,0 +1,27 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  // Proxy for backend server
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://172.20.10.2:3001',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    })
+  );
+
+  // Proxy for Arduino server
+  app.use(
+    '/arduino',
+    createProxyMiddleware({
+      target: 'http://172.20.10.9:3000',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/arduino': ''
+      }
+    })
+  );
+};
